275dfa5a9ff66f8ef36fc163ae383f6b9a060cea
Deepansh Gupta <deepanshgupta@MacBook-Air.local>
2025-06-04 15:51:59 +0530

    final

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router";
 import { ID } from "appwrite";
 
 const AuthContext = createContext();
-
 export const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState(null);
@@ -27,7 +26,7 @@ export const AuthProvider = ({ children }) => {
         console.log("CREDS:", credentials);
 
         try {
-            let response = await account.createEmailPasswordSession(
+            let response = await account.createSession(
                 credentials.email,
                 credentials.password
             );
@@ -46,33 +45,35 @@ export const AuthProvider = ({ children }) => {
 
     const handleRegister = async (e, credentials) => {
         e.preventDefault();
-        console.log("Handle Register triggered!", credentials);
-
+      
         if (credentials.password1 !== credentials.password2) {
-            alert("Passwords did not match!");
-            return;
+          alert("Passwords did not match!");
+          return;
         }
-
+      
         try {
-            let response = await account.create(
-                ID.unique(),
-                credentials.email,
-                credentials.password1,
-                credentials.name
-            );
-            console.log("User registered!", response);
-
-            await account.createEmailSession(
-                credentials.email,
-                credentials.password1
-            );
-            let accountDetails = await account.get();
-            setUser(accountDetails);
-            navigate("/");
+          const response = await account.create(
+            ID.unique(),
+            credentials.email,
+            credentials.password1,
+            credentials.name
+          );
+          console.log("User registered!", response);
+      
+          await account.createSession(credentials.email, credentials.password1);
+          let accountDetails = await account.get();
+          setUser(accountDetails);
+          navigate("/");
         } catch (error) {
+          if (error.code === 409) {
+            alert("User with this email already exists. Please login.");
+          } else {
             console.error(error);
+            alert("Something went wrong during registration.");
+          }
         }
-    };
+      };
+      
 
     const contextData = {
         user,
